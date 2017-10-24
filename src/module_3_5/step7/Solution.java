2e1ec359b4c9605c8d93fa7e172797e99f4a2cd0
h-xDF <ypereyaslov@parseq.pro>
2017-10-24 17:54:32 +0300

    solved 3.5 - 7 step

---
+++ src/module_3_5/step7/Solution.java
@@ -0,0 +1,43 @@
+package module_3_5.step7;
+
+import java.util.function.DoubleUnaryOperator;
+
+/**
+ * Created by ypereyaslov on 24.10.2017.
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+
+        double a = 0;
+        double b = 10;
+        DoubleUnaryOperator fun = x -> 1;
+
+        System.out.println(integrate(fun, a, b));
+    }
+
+    public static double integrate(DoubleUnaryOperator f, double a, double b) {
+
+        double exp = 1e-1;
+        double sumBefore = 1d;
+        double sumAfter = 0d;
+        double delta = 1e-13;
+
+        while (delta <= Math.abs(sumAfter - sumBefore)) {
+
+            sumAfter = sumBefore;
+            sumBefore = 0;
+            double start = a;
+
+            while (start <= b) {
+
+                start = start + exp;
+                sumBefore += f.applyAsDouble(start) * exp;
+            }
+            exp =  exp / 2.0;
+        }
+
+        System.out.println("exp = " + exp);
+        return sumBefore;
+    }
+}
---
+++ src/module_3_5/step8/Solution.java
@@ -0,0 +1,15 @@
+package module_3_5.step8;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        byte[] example = {72, 101, 108, 108, 111, 33};
+        AsciiCharSequence answer = new AsciiCharSequence(example);
+        System.out.println("Последовательность - " + answer.toString());//Hello!
+        System.out.println("Размер её - " + answer.length());//6
+        System.out.println("Символ под № 1 - " + answer.charAt(1));//e
+        System.out.println("Подпоследовательность - " + answer.subSequence(1, 5));//ello
+
+    }
+}
